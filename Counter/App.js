1cbe4c87e2e9e2f3c2531d43ee87d82a0d7f9dfd
elijah <elijah.blake@aggiemail.usu.edu>
2020-01-23 22:08:07 -0700

    got two bottons on the screen

--- Counter/App.js
+++ Counter/App.js
@@ -1,12 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
+import UpButton from './components/upButton';
 import {
   SafeAreaView,
   StyleSheet,
@@ -24,91 +17,52 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+export default class App extends React.Component {
+  styles = StyleSheet.create({
+    MainContainer: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: Colors.black,
+    },
+    scrollView: {
+      backgroundColor: Colors.black,
+    },
+    sectionContainer: {
+      marginTop: 32,
+      paddingHorizontal: 24,
+      borderWidth: 8,
+      borderColor: Colors.white,
+    },
+    sectionTitle: {
+      fontSize: 24,
+      fontWeight: '600',
+      color: Colors.white,
+    },
+  });
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+  increment = () => {
+    this.setState((state) => {
+      console.log("increment button");
+    });
+  }
 
-export default App;
+  render() {
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView style={this.styles.MainContainer}>
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={this.styles.scrollView}>
+            <View style={this.styles.sectionContainer}>
+              <Text style={this.styles.sectionTitle}>Step One</Text>
+            </View>
+            <UpButton title="+" onPress={this.increment}/>
+            <UpButton title="-" onPress={this.increment}/>
+          </ScrollView>
+        </SafeAreaView>
+      </>
+    );
+  }
+}
---
+++ Counter/components/upButton.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import {TouchableOpacity, View, Text, StyleSheet} from 'react-native';
+
+export default class UpButton extends React.Component {
+  styles = StyleSheet.create({
+    button: {
+      backgroundColor: 'black',
+      margin: 16,
+      borderRadius: 96,
+      padding: 24,
+      width: 146,
+      height: 146,
+      alignSelf: 'center',
+      borderWidth: 8,
+      borderColor: 'white',
+    },
+    title: {
+      fontSize: 64,
+      alignSelf: 'center',
+      color: 'white',
+    },
+  });
+
+  render() {
+    return (
+      <TouchableOpacity onPress={this.props.onPress}>
+        <View style={[this.styles.button, this.props.style]}>
+          <Text style={[this.styles.title, this.props.titleStyle]}>{this.props.title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
